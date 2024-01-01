c6a49e6b18193c91d8f5db56c577a302fadb4a99
Xiaohui <xiaohui.man@bd.com>
2024-01-01 12:08:24 +0800

    add all bash scripts

---
+++ code/get_GHCNDdata_master.sh
@@ -0,0 +1,3 @@
+#!/bin/bash 
+file=$1
+wget -P data/ https://www.ncei.noaa.gov/pub/data/ghcn/daily/$file
---
+++ driver.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sh code/get_GHCNDdata_master.sh  ghcnd_all.tar.gz
+sh code/get_GHCNDdata_master.sh  ghcnd-inventory.txt
+sh code/get_GHCNDdata_master.sh  ghcnd-stations.txt
+
+## From CC247: https://www.youtube.com/watch?v=Ft8ayhfgaqo 
